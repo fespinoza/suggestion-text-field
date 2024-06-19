d69ee0351d735c8356c85b01d9d8be431af19c98
Felipe Espinoza <fespinozacast@gmail.com>
2024-06-19 22:02:17 +0200

    Update package name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SuggestionTextField",
+    name: "suggestion-text-field",
     platforms: [.macOS(.v14)],
     products: [
         .library(
